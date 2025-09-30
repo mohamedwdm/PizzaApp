1543ea1be48943999e4e7b70a91a7df77fec9c9a
mohamedwdm <wdm46857@gmail.com>
2025-09-30 12:55:30 +0300

    create user repo and impl

---
+++ lib/features/auth/data/models/repos/user_repo.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:pizza_app/core/errors/failure.dart';
+import 'package:pizza_app/features/auth/data/models/user_model.dart';
+
+abstract class UserRepo {
+  Future<Either<Failure , void>> registerUser({
+    required String email,
+    required String password,
+    required String name,
+    required bool hasActiveCart,
+  });
+  Future<Either<Failure , UserModel>> loginUser({required String email, required String password});
+  Future<void> logOut();
+}
--- lib/features/auth/presentation/manager/auth_cubit/auth_cubit.dart
+++ lib/features/auth/presentation/manager/auth_cubit/auth_cubit.dart
@@ -1,14 +1,14 @@
-import 'dart:developer';
-
 import 'package:bloc/bloc.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:pizza_app/features/auth/data/models/repos/user_repo.dart';
 import 'package:pizza_app/features/auth/data/models/user_model.dart';
 
 part 'auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState> {
-  AuthCubit() : super(AuthInitial());
+  AuthCubit(this.userRepo) : super(AuthInitial());
+
+  final UserRepo userRepo;
 
   Future<void> registerUser({
     required String email,
@@ -17,41 +17,20 @@ class AuthCubit extends Cubit<AuthState> {
     required bool hasActiveCart,
   }) async {
     emit(RegisterLoading());
-    try {
-      // Step 1: Create account in Firebase Auth
-      UserCredential userCredential = await FirebaseAuth.instance
-          .createUserWithEmailAndPassword(email: email, password: password);
-
-      User? user = userCredential.user;
-
-      // Step 2: Save user profile in Firestore
-      if (user != null) {
-        await FirebaseFirestore.instance.collection('users').doc(user.uid).set({
-          'userid': user.uid,
-          'name': name,
-          'email': email,
-          'hasActiveCart': hasActiveCart,
-          'createdAt': FieldValue.serverTimestamp(),
-        });
-      }
-
-      log("✅ User registered & profile saved!");
-      emit(RegisterSuccess());
-    } on FirebaseAuthException catch (e) {
-      if (e.code == 'weak-password') {
-        emit(
-          RegisterFailure(errorMessege: "The password provided is too weak."),
-        );
-      } else if (e.code == 'email-already-in-use') {
-        emit(
-          RegisterFailure(
-            errorMessege: 'The account already exists for that email.',
-          ),
-        );
-      }
-    } on Exception catch (e) {
-      emit(RegisterFailure(errorMessege: "Something went wrong"));
-    }
+    var result = await userRepo.registerUser(
+      email: email,
+      password: password,
+      name: name,
+      hasActiveCart: hasActiveCart,
+    );
+    result.fold(
+      (failure) {
+        emit(RegisterFailure(errorMessege: failure.errorMessage));
+      },
+      (_) {
+        emit(RegisterSuccess());
+      },
+    );
   }
 
   Future<void> loginUser({
@@ -59,45 +38,18 @@ class AuthCubit extends Cubit<AuthState> {
     required String password,
   }) async {
     emit(LoginLoading());
-    try {
-      // Step 1: Sign in with Firebase Auth
-      UserCredential userCredential = await FirebaseAuth.instance
-          .signInWithEmailAndPassword(email: email, password: password);
-
-      User? user = userCredential.user;
-
-      if (user != null) {
-        log("✅ Signed in as: ${user.email}");
-
-        // Step 2: Get user profile from Firestore (optional)
-        DocumentSnapshot userDoc =
-            await FirebaseFirestore.instance
-                .collection('users')
-                .doc(user.uid)
-                .get();
-
-        emit(LoginLoading());
-        if (userDoc.exists) {
-          log("📂 User profile: ${userDoc.data()}");
-          emit(LoginSuccess(user: UserModel.fromJson(userDoc)));
-        } else {
-          log("⚠️ No profile found in Firestore!");
-        }
-      }
-    } on FirebaseAuthException catch (ex) {
-      if (ex.code == 'user-not-found') {
-        emit(LoginFailure(errorMessege: "No user found for that email"));
-      } else if (ex.code == 'wrong-password') {
-        emit(
-          LoginFailure(errorMessege: 'Wrong password provided for that user.'),
-        );
-      }
-    } on Exception catch (e) {
-      emit(LoginFailure(errorMessege: "Something went wrong"));
-    }
+    var result = await userRepo.loginUser(email: email, password: password);
+    result.fold(
+      (failure) {
+        emit(LoginFailure(errorMessege: failure.errorMessage));
+      },
+      (user) {
+        emit(LoginSuccess(user: user));
+      },
+    );
   }
 
   Future<void> logOut() async {
-    await FirebaseAuth.instance.signOut();
+    await userRepo.logOut();
   }
 }
