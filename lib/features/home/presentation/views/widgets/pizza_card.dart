a6a48dd1266b946e9e4185e006a62cf7abe29285
mohamedwdm <wdm46857@gmail.com>
2025-10-04 13:56:25 +0300

    fix render flex

--- lib/features/home/presentation/views/widgets/pizza_card.dart
+++ lib/features/home/presentation/views/widgets/pizza_card.dart
@@ -19,79 +19,85 @@ class PizzaCard extends StatelessWidget {
       child: InkWell(
         borderRadius: BorderRadius.circular(20),
         onTap: (){},
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.asset('assets/images/1.png'),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 6.0),
-              child: Row(
-                children: [
-                  CustomPizzaItemLabel(
-                    color: Colors.red.shade800,
-                    text: 'NON-VEG',
-                    textColor: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  const SizedBox(width: 8),
-                   CustomPizzaItemLabel(
-                    color: Colors.green.withValues(alpha: 0.2),
-                    text: '🌶 BALANCE',
-                    textColor: Colors.green,
-                    fontWeight: FontWeight.w800,
-                  ),
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+             mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset('assets/images/1.png'),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 6.0),
+                child: Row(
+                  children: [
+                    CustomPizzaItemLabel(
+                      color: Colors.red.shade800,
+                      text: 'NON-VEG',
+                      textColor: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    const SizedBox(width: 8),
+                     CustomPizzaItemLabel(
+                      color: Colors.green.withValues(alpha: 0.2),
+                      text: '🌶 BALANCE',
+                      textColor: Colors.green,
+                      fontWeight: FontWeight.w800,
+                    ),
+                  ],
+                ),
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 6.0),
-              child: Text(
-                'Chessy Marvel',
-                style: TextStyle(fontWeight: FontWeight.w900, fontSize: 20),
+              SizedBox(height: 2,),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 6.0),
+                child: Text(
+                  'Chessy Marvel',
+                  style: TextStyle(fontWeight: FontWeight.w900, fontSize: 20),
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 6.0),
-              child: Text(
-                'Crafting joy: your pizza, your rules, best taster',
-                style: TextStyle(fontSize: 12, color: Colors.grey.shade700),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 6.0),
+                child: Text(
+                  'Crafting joy: your pizza, your rules, best taster',
+                  style: TextStyle(fontSize: 12, color: Colors.grey.shade700),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 6.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      const Text(
-                        '\$12.00',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.lightBlueAccent,
-                          fontWeight: FontWeight.w700,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 6.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        const Text(
+                          '\$12.00',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.lightBlueAccent,
+                            fontWeight: FontWeight.w700,
+                          ),
                         ),
-                      ),
-                      const SizedBox(width: 5),
-                      Text(
-                        '\$16.00',
-                        style: TextStyle(
-                          fontSize: 12,
-                          color: Colors.grey.shade700,
-                          fontWeight: FontWeight.w700,
-                          decoration: TextDecoration.lineThrough,
+                        const SizedBox(width: 5),
+                        Text(
+                          '\$16.00',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey.shade700,
+                            fontWeight: FontWeight.w700,
+                            decoration: TextDecoration.lineThrough,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(CupertinoIcons.add_circled_solid),
-                  ),
-                ],
+                      ],
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(CupertinoIcons.add_circled_solid),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
